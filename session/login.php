081069f17ddfe0e1e72036736e690ad44390d8b9
hebertphp <hebertphp@gmail.com>
2019-04-24 10:58:48 -0300

    Update login.php

--- session/login.php
+++ session/login.php
@@ -1,3 +1,21 @@
 <?php
-#arquivo de login
+session_start();
+$_SESSION=array();
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+ <title>Login</title>
+</head>
+<body>
+<form method="post" action="recebe.php">
+Login <input type="text" name="login" value=""><br>
+Senha <input type="text" name="senha" value=""><br>
+<input type="submit" value="Enviar">
+</form>
+<?php
+ echo md5("uninove");
+ ?>
+</body>
+</html
