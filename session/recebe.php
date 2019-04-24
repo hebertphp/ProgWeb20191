d32e4a7d26e4c8aac0fda86e02eb3350195891f4
hebertphp <hebertphp@gmail.com>
2019-04-24 11:04:08 -0300

    Update recebe.php

--- session/recebe.php
+++ session/recebe.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
-$login=$_POST["login"];
-$senha=$_POST["senha"];
+if ($_SERVER["REQUEST_METHOD"]!="POST"){
+    header("location: login.php");
+    exit();
+}
+
+$login = filter_input(INPUT_POST, 'login',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$senha = filter_input(INPUT_POST, 'senha',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 if($login=='aluno' && md5($senha)=='02bb12fbbc1dcf408d7dab12f43f5cf6'){
     $_SESSION['login']=$login;
